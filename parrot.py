628da9814b523f679124a7eac509c6b3b426c4ea
Ivan Lifanov <letalbark@gmail.com>
2023-04-30 14:31:14 +0700

    norm

---
+++ parrot.py
@@ -0,0 +1,14 @@
+def handler(event, context):
+    text = 'Привет!\nЯ повторю все, что ты скажешь мне!'
+    if 'request' in event and \
+            'original_utterance' in event['request'] \
+            and len(event['request']['original_utterance']) > 0:
+        text = event['request']['original_utterance']
+    return {
+        'version': event['version'],
+        'session': event['session'],
+        'response': {
+            'text': text,
+            'end_session': 'false'
+        },
+    }
